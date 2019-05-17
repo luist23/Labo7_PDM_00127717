c6c67fe238082de68a929f1f296997f8282ec7ef
luist23 <00127717@uca.edu.sv>
2019-05-17 17:22:59 -0600

    add adapter

--- app/src/main/java/com/luist23/room_project/adapters/RepoAdapter.kt
+++ app/src/main/java/com/luist23/room_project/adapters/RepoAdapter.kt
@@ -1,4 +1,35 @@
 package com.luist23.room_project.adapters
 
-class RepoAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.luist23.room_project.R
+import com.luist23.room_project.database.entities.GithubRepo
+import kotlinx.android.synthetic.main.repo_cardview.view.*
+
+class RepoAdapter(var repos:List<GithubRepo>) : RecyclerView.Adapter<RepoAdapter.ViewHolder>(){
+    override fun getItemCount(): Int {
+        return repos.size
+    }
+
+    fun updateList(newRepos:List<GithubRepo>){
+        this.repos = newRepos
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(repos[position])
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.repo_cardview,parent,false)
+        return  ViewHolder(view)
+    }
+
+    class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+        fun bind(repo:GithubRepo) = with(itemView){
+            this.tv_repo_name.text = repo.name
+        }
+    }
 }
